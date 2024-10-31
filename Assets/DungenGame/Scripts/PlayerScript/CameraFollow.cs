62c1012fda2ff5be933721565f0a58ea3918e0a8
Mohit Chaudhary <mohitchaudhary@techupware.com>
2024-11-01 01:35:56 +0530

    Added NamesSpace to the scripts

---
+++ Assets/DungenGame/Scripts/PlayerScript/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MVDEV.DungeonGame.Scripts.PlayerScripts
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        public Transform playerTransform;
+        public float speed;
+
+        private void Update()
+        {
+            if (playerTransform == null)
+            {
+                return;
+            }
+            transform.position = Vector2.Lerp(transform.position, new Vector2(playerTransform.position.x, playerTransform.position.y), speed);
+        }
+    }
+}
