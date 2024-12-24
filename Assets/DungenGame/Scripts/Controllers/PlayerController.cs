9672fcd7eca74d20605ed09617a29f74bb7c6bc8
Mohit Chaudhary <mohit45chaudhary@gmail.com>
2024-12-24 19:34:20 +0530

    [DSC-9] created Basic Player Controller

--- Assets/DungenGame/Scripts/Controllers/PlayerController.cs
+++ Assets/DungenGame/Scripts/Controllers/PlayerController.cs
@@ -5,32 +5,71 @@ using Zenject;
 
 namespace MVDEV.DungeonGame.Scripts.Controllers
 {
-    public class PlayerController: MonoBehaviour
+    public class PlayerController : MonoBehaviour
     {
-        [Inject]private PlayerModel _playerModel;
-        [Inject]private PlayerView _playerView;
+        [Inject] private PlayerModel _playerModel;
+        [Inject] private PlayerView _playerView;
 
+        [SerializeField] private float _moveSpeed = 5f;
+
+        private float _maxHunger = 100;
+        private float _maxStamina = 100;
+        private float _maxHealth = 100;
 
         private void Update()
         {
             HandleInput();
+            UpdateStats();
         }
 
+        private void Start()
+        {
+            Initialize();
+        }
         private void HandleInput()
         {
+            // Handle movement
             float horizontal = Input.GetAxis("Horizontal");
             float vertical = Input.GetAxis("Vertical");
-            Vector2 movement = new Vector2(horizontal, vertical) * Time.deltaTime * 5;
+            Vector2 movement = new Vector2(horizontal, vertical) * _moveSpeed * Time.deltaTime;
 
             // Update player position
             _playerView.UpdatePosition((Vector2)transform.position + movement);
 
-            // Example: Simulate taking damage
-            if (Input.GetKeyDown(KeyCode.Space))
+            // Example: Simulate taking damage with spacebar
+            if (Input.GetKeyDown(KeyCode.K))
             {
                 _playerModel.TakeDamage(10);
                 _playerView.UpdateHealth(_playerModel.Health, 100);
             }
+        }
+        private void UpdateStats()
+        {
+            // Reduce hunger over time
+            _playerModel.DecreaseHunger(Time.deltaTime * 0.002f);
+
+            // Reduce stamina if moving
+            if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+            {
+                _playerModel.ConsumeStamina(Time.deltaTime * 1f);
+            }
+
+            // If hunger is 0, reduce health
+            if (_playerModel.Hunger <= 0)
+            {
+                _playerModel.TakeDamage(Time.deltaTime * 0.5f);
+            }
+
+            // Update UI or visual representation for stats
+            _playerView.UpdateHealth(_playerModel.Health, _maxHealth);
+            _playerView.UpdateStamina(_playerModel.Stamina, _maxStamina);
+            _playerView.UpdateHunger(_playerModel.Hunger, 100);
+        }
+
+        public void Initialize()
+        {
+            _playerModel.Init(_maxHealth, _maxStamina, _maxHunger);
+            _playerView.InitialiePlayerStats(1, 1, 1);
         }
     }
 }
--- Assets/DungenGame/Scripts/Views/PlayerView.cs
+++ Assets/DungenGame/Scripts/Views/PlayerView.cs
@@ -1,4 +1,6 @@
+using MVDEV.DungeonGame.Scripts.Controllers;
 using UnityEngine;
+using Zenject;
 
 namespace MVDEV.DungeonGame.Scripts.Views
 {
@@ -6,12 +8,14 @@ namespace MVDEV.DungeonGame.Scripts.Views
     {
         public GameObject playerSprite;
         public Animator animator;
+        [Inject] private PlayerStatsController _playerStatsController;
 
         public void UpdateHealth(float currentHealth, float maxHealth)
         {
             Debug.Log($"Player Health {currentHealth}");
             //create UI for Health bar and a script that sets UI
             //healthBar.SetHealth(currentHealth/ maxHealth) //value will be from 0 to 1
+            _playerStatsController.SetHealth(currentHealth / maxHealth);
         }
 
         public void UpdatePosition(Vector2 position)
@@ -23,5 +27,26 @@ namespace MVDEV.DungeonGame.Scripts.Views
         {
             //TBD
         }*/
+
+        public void UpdateStamina(float currentStamina, float maxStamina)
+        {
+            Debug.Log($"Stamina: {currentStamina}/{maxStamina}");
+            _playerStatsController.SetStamina(currentStamina/maxStamina);
+            // Update stamina UI here
+        }
+
+        public void UpdateHunger(float currentHunger, float maxHunger)
+        {
+            Debug.Log($"Hunger: {currentHunger}/{maxHunger}");
+            // Update hunger UI here
+            _playerStatsController.SetHunger(currentHunger / maxHunger);
+        }
+
+        public void InitialiePlayerStats(float currentHealth, float currentStamina, float currentHunger)
+        {
+            _playerStatsController.SetHealth(currentHealth);
+            _playerStatsController.SetHunger(currentHunger);
+            _playerStatsController.SetStamina(currentStamina);
+        }
     }
 }
